1cfd78241c36b6e870e4191579f0fcf1405cdec8
niyueming <niyueming@163.com>
2014-09-25 15:56:15 +0800

    初始导入

---
+++ app/src/main/java/net/nym/extendcomponent/broadcastreceiver/NetBroadcastReceiver.java
@@ -0,0 +1,87 @@
+package net.nym.extendcomponent.broadcastreceiver;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+
+
+/**
+ * 类 <code>NetBroadcastReceiver</code>
+ * 监听网络状态变化
+ * 
+ * @author nym
+ * @version 2013-6-4
+ * @since 
+ * @time 上午9:23:32
+ * @see 
+ */
+public class NetBroadcastReceiver extends BroadcastReceiver {
+
+	private final String TAG = NetBroadcastReceiver.class.getSimpleName();
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+//		Log.i(TAG + ":%s", "网络状态改变");
+
+		boolean success = false;
+		boolean wifiState = false;
+		boolean mobileState = false;
+		String mobileNetName = "";	//网络名称
+
+		// 获得网络连接服务
+		ConnectivityManager connManager = (ConnectivityManager) arg0
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+//		connManager.setNetworkPreference(ConnectivityManager.TYPE_WIFI);	//优先使用wifi
+		NetworkInfo  wifiInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if(wifiInfo != null)
+		{
+			if(wifiInfo.isConnected())
+			{
+				wifiState = true;
+			}
+		}
+		
+		NetworkInfo  mobileInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		if(mobileInfo != null)
+		{
+			if(mobileInfo.isConnected())
+			{
+				mobileState = true;
+			}
+			switch (mobileInfo.getSubtype()) {
+			case TelephonyManager.NETWORK_TYPE_UMTS:
+			case TelephonyManager.NETWORK_TYPE_HSDPA:
+				mobileNetName = "联通3G";
+				break;
+			case TelephonyManager.NETWORK_TYPE_GPRS:
+			case TelephonyManager.NETWORK_TYPE_EDGE:
+				mobileNetName = "移动或联通2G";
+				break;
+			case TelephonyManager.NETWORK_TYPE_CDMA:
+				mobileNetName = "电信2G";
+				break;
+			case TelephonyManager.NETWORK_TYPE_EVDO_0:
+			case TelephonyManager.NETWORK_TYPE_EVDO_A:
+			case TelephonyManager.NETWORK_TYPE_EVDO_B:
+				mobileNetName = "电信3G";
+				break;
+			case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+				mobileNetName = "UNKNOWN";
+				break;
+			default:
+				break;
+			}
+		}
+		
+		if (wifiState | mobileState) { // 判断是否正在使用WIFI网络
+			success = true;
+		}
+//		Log.i(TAG + ":%s,%s=%s", "网络状态改变:" + success,mobileNetName,mobileState + "");
+//		OperateSharePreferences.getInstance().saveNetState(success);
+
+	}
+
+}

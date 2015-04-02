a98567bf215b0a7bbd7a31d2deac90cad746d6bc
niyueming <niyueming@163.com>
2015-04-02 15:26:50 +0800

    实现Entities可foreach

--- app/src/main/java/net/nym/extendcomponent/entity/Entities.java
+++ app/src/main/java/net/nym/extendcomponent/entity/Entities.java
@@ -1,6 +1,9 @@
 package net.nym.extendcomponent.entity;
 
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * 类Entities
@@ -11,7 +14,7 @@ import java.util.ArrayList;
  * @time 下午6:16:15
  * @see Entity
  */
-public class Entities<T extends Entity> {
+public class Entities<T extends Entity> implements Iterable<T>{
     ArrayList<T> list;
 
     public Entities() {
@@ -51,6 +54,12 @@ public class Entities<T extends Entity> {
         list.remove(index);
     }
 
+    public T remove(int position) {
+        return list.remove(position);
+    }
+    public boolean remove(T object) {
+        return list.remove(object);
+    }
 
     @Override
     public String toString() {
@@ -62,5 +71,62 @@ public class Entities<T extends Entity> {
             sb.deleteCharAt(sb.length() - 1);
         sb.append("]");
         return sb.toString();
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new SimpleListIterator();
+    }
+
+    private class SimpleListIterator implements Iterator<T> {
+        int pos = -1;
+
+        int expectedModCount;
+
+        int lastPosition = -1;
+
+        SimpleListIterator() {
+            expectedModCount = Entities.this.size();
+        }
+
+        public boolean hasNext() {
+            return pos + 1 < size();
+        }
+
+        public T next() {
+            if (expectedModCount == Entities.this.size()) {
+                try {
+                    T result = get(pos + 1);
+                    lastPosition = ++pos;
+                    return result;
+                } catch (IndexOutOfBoundsException e) {
+                    throw new NoSuchElementException();
+                }
+            }
+            throw new ConcurrentModificationException();
+        }
+
+        public void remove() {
+            if (this.lastPosition == -1) {
+                throw new IllegalStateException();
+            }
+
+            if (expectedModCount != Entities.this.size()) {
+                throw new ConcurrentModificationException();
+            }
+
+            try {
+                Entities.this.remove(lastPosition);
+            } catch (IndexOutOfBoundsException e) {
+                throw new ConcurrentModificationException();
+            }
+
+            expectedModCount = Entities.this.size();
+            if (pos == lastPosition) {
+                pos--;
+            }
+            lastPosition = -1;
+        }
     }
 }
